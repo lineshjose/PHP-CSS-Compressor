1b4928fcb036b9008df74968df2890846e8b5a30
Linesh Jose <lineshjose@gmail.com>
2016-05-19 16:34:12 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,11 +4,10 @@
 	Description: A simple PHP functions that compress css codes
 	Version : 2.00
 	Author: Linesh Jose
-	Url: https://lineshjose.com
-	Email: lineshjose@gmail.com
-	Donate:  https://lineshjose.com/make-a-donation/
+	Url: http://linesh.com
+	Donate:  http://linesh.com/make-a-donation/
 	github: https://github.com/lineshjose
-	Demo: https://lineshjose.com/blog/how-to-create-a-simple-css-compressor-using-php/
+	Demo: http://linesh.com/blog/how-to-create-a-simple-css-compressor-using-php/
 	Copyright: Copyright (c) 2015 LineshJose.com
 	
 	Note: This script is free; you can redistribute it and/or modify  it under the terms of the GNU General Public License as published by 	the Free Software Foundation; either version 2 of the License, or (at your option) any later version.This script is distributed in the hope 	that it will be useful,    but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the  GNU General Public License for more details.
